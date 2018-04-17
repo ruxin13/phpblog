c950632140b196dd376043b2e43f8d2b3682d589
lishu <lishu@bgi.com>
2018-04-17 20:43:42 +0800

    default: 去掉底部的版权信息

--- zb_users/theme/default/template/footer.php
+++ zb_users/theme/default/template/footer.php
@@ -1,6 +1,5 @@
 {* Template Name:公共底部 *}
 		<div id="divBottom">
-			<h4 id="BlogPowerBy">Powered By {$zblogphphtml}</h4>
 			<h3 id="BlogCopyRight">{$copyright}</h3>
 		</div><div class="clear"></div>
 	</div><div class="clear"></div>
