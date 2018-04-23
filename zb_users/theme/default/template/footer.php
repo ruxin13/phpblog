70639c4a3e7fdfeb58b703c81fdbed5ff6da3ae6
lishu <lishu@bgi.com>
2018-04-23 08:51:11 +0800

    .idea

--- zb_users/theme/default/template/footer.php
+++ zb_users/theme/default/template/footer.php
@@ -1,6 +1,6 @@
 {* Template Name:公共底部 *}
 		<div id="divBottom">
-			<h3 id="BlogCopyRight">{$copyright}</h3>
+			<h3 id="BlogCopyRight"><a href="http://www.miitbeian.gov.cn">{$copyright}</a></h3>
 		</div><div class="clear"></div>
 	</div><div class="clear"></div>
 	</div><div class="clear"></div>
