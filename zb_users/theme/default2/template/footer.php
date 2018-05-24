519bcb902551ba4a279a23c3369656fefbfee103
lishu <lishu@bgi.com>
2018-05-24 10:35:38 +0800

    .gitignore

---
+++ zb_users/theme/default2/template/footer.php
@@ -0,0 +1,10 @@
+{* Template Name:公共底部 *}
+		<div id="divBottom">
+			<h3 id="BlogCopyRight"><a href="http://www.miitbeian.gov.cn">{$copyright}</a></h3>
+		</div><div class="clear"></div>
+	</div><div class="clear"></div>
+	</div><div class="clear"></div>
+</div>
+{$footer}
+</body>
+</html>
